67d91adba0da77812edf6be4238ffefcb393816a
L1ndseyHerman <lherman21@jcu.edu>
2021-10-13 15:16:36 -0400

    Caught up to vid435: loading spinner, error module, and a review of state batching (1 render per 2 state updates same method).

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,16 +2,20 @@ import React, { useState, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
   }, []);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch("https://udemy-reacthooks-review-default-rtdb.firebaseio.com/.json", {
       method: "POST",
       //  JSON can .stringify() an object OR an array!
@@ -21,6 +25,7 @@ const Ingredients = () => {
       },
     })
       .then((response) => {
+        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
@@ -35,6 +40,7 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
     //  Need backticks to choose which item to delete:
     //  Hmm, my Firebase doesn't have an "ingredients" section. Is that why the filter doesn't work?
     fetch(
@@ -43,16 +49,35 @@ const Ingredients = () => {
       {
         method: "DELETE",
       }
-    ).then((response) => {
-      setUserIngredients((prevIngredients) =>
-        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-      );
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        setUserIngredients((prevIngredients) =>
+          prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        );
+      })
+      .catch((error) => {
+        //setError(error.message);
+
+        //  This is state batching. U wouldn't be able to access the results from
+        //  setError() in setIsLoading(), the states update at the same time somehow bec threads.
+        //  Both of these only call 1 render instead of 2!
+        setIsLoading(false);
+        setError("Something went wrong!");
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
