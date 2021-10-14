e91c74582d7dac3d80acda6c5aebaceb731dc1d3
L1ndseyHerman <lherman21@jcu.edu>
2021-10-13 22:59:49 -0400

    Caught up to vid443: making sure the addIngredientHandler() and the filteredIngredientsHandler() both don't work, or at least that's what I did this video :(

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -22,8 +22,15 @@ const ingredientReducer = (currentIngredients, action) => {
 
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const { isLoading, error, data, sendRequest, reqExtra, reqIdentifier } =
-    useHttp();
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifier,
+    clear,
+  } = useHttp();
 
   //const [userIngredients, setUserIngredients] = useState([]);
   //const [isLoading, setIsLoading] = useState(false);
@@ -55,32 +62,6 @@ const Ingredients = () => {
         ingredient,
         "ADD_INGREDIENT"
       );
-      /*dispatchHttp({ type: "SEND" });
-    fetch("https://udemy-reacthooks-review-default-rtdb.firebaseio.com/.json", {
-      method: "POST",
-      //  JSON can .stringify() an object OR an array!
-      body: JSON.stringify(ingredient),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        dispatchHttp({ type: "RESPONSE" });
-        return response.json();
-      })
-      .then((responseData) => {
-        //  Ingredient is already an object, so putting an object inside of an object! ...ingredient gets
-        //  all of its properties.
-        /*setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          //  This gets the auto-generated id from Firebase!
-          { id: responseData.name, ...ingredient },
-        ]);
-        dispatch({
-          type: "ADD",
-          ingredient: { id: responseData.name, ...ingredient },
-        });
-      });*/
     },
     [sendRequest]
   );
@@ -101,10 +82,6 @@ const Ingredients = () => {
     [sendRequest]
   );
 
-  const clearError = useCallback(() => {
-    //dispatchHttp({ type: "CLEAR" });
-  }, []);
-
   const ingredientList = useMemo(() => {
     return (
       <IngredientList
@@ -116,7 +93,7 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading}
